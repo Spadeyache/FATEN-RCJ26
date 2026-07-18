0356c62dcd0e737dca67447b94603fec25d9efef
Spadeyache <magic.global.k@gmail.com>
2026-07-18 15:18:55 -0700

    clean up

--- teensy/STS-imuMerge/IcsClass_V300/ICS_ArduinoFunctonListV3_0_0(See to index html)/search/classes_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['icsbaseclass_0',['IcsBaseClass',['../class_ics_base_class.html',1,'']]],
-  ['icshardserialclass_1',['IcsHardSerialClass',['../class_ics_hard_serial_class.html',1,'']]]
-];
--- teensy/STS-imuMerge/IcsClass_V300/ICS_ArduinoFunctonListV3_0_0(See to index html)/search/variables_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['baudrate_0',['baudRate',['../class_ics_hard_serial_class.html#a1bb994dd57372ad8a311bfae212cc670',1,'IcsHardSerialClass']]]
-];
